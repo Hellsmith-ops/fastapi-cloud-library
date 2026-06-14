38b91134bc70328d927eebe36494e702730db3ed
Duc Tran <dtran128@wgu.com>
2026-06-13 17:17:06 -0700

    Fixed typo in test

--- tests/test_health.py
+++ tests/test_health.py
@@ -3,7 +3,7 @@ from main import app
 
 client = TestClient(app)
 
-def test_healthz():
-    response = client.get("/healthz")
+def test_health():
+    response = client.get("/health")
     assert response.status_code == 200
     assert response.json() == {"status": "ok"}
